75178c45758bd4ce1339096a43726b0def4ddacf
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-11-11 12:04:13 -0300

    feat: home page creation and styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,58 @@
+import { Play } from "@phosphor-icons/react";
+import { 
+  CountdownContainer, 
+  FormContainer, 
+  HomeContainer, 
+  MinutesAmountInput, 
+  StartCountdownButton, 
+  TaskInput 
+} from "./styles";
+
 export function Home() {
   return (
-    <h1>Home</h1>
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+         
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput 
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
